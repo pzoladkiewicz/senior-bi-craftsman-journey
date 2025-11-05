d31b981280a10f5fef56212478741ea3db6c6bc1
Paweł Żołądkiewicz <pzoladkiewicz@gmail.com>
2025-11-05 10:45:13 +0100

    SQL: zmiana precyzji wartości decimal (38, 10) -> (18, 4)

--- projects/e-commerce-order-analytics/sql/01_staging_schema.sql
+++ projects/e-commerce-order-analytics/sql/01_staging_schema.sql
@@ -1,11 +1,12 @@
 USE SupplyChainDB;
 GO
+
 CREATE TABLE staging.DataCo_Raw (
      Type                    nvarchar(50)     
     ,Days_for_shipping_real  int              
     ,Days_for_shipment_sched int              
-    ,Benefit_per_order       decimal(38,10)    
-    ,Sales_per_customer      decimal(38,10)    
+    ,Benefit_per_order       decimal(18, 4)    
+    ,Sales_per_customer      decimal(18, 4)    
     ,Delivery_Status         nvarchar(50)     
     ,Late_delivery_risk      bit              
     ,Category_Id             int              
@@ -32,15 +33,15 @@ CREATE TABLE staging.DataCo_Raw (
     ,Order_Date              datetime           -- order date (DateOrders)
     ,Order_Id                int              
     ,Order_Item_Cardprod_Id  int              
-    ,Order_Item_Discount     decimal(38,10)    
-    ,Order_Item_Discount_Rate decimal(38,10)   
+    ,Order_Item_Discount     decimal(18, 4)    
+    ,Order_Item_Discount_Rate decimal(9, 6)   
     ,Order_Item_Id           int              
-    ,Order_Item_Product_Price decimal(38,10)   
-    ,Order_Item_Profit_Ratio decimal(38,10)    
+    ,Order_Item_Product_Price decimal(18, 4)   
+    ,Order_Item_Profit_Ratio decimal(9, 6)    
     ,Order_Item_Quantity     int              
-    ,Sales                   decimal(38,10)    
-    ,Order_Item_Total        decimal(38,10)    
-    ,Order_Profit_Per_Order  decimal(38,10)    
+    ,Sales                   decimal(18, 4)    
+    ,Order_Item_Total        decimal(18, 4)    
+    ,Order_Profit_Per_Order  decimal(18, 4)    
     ,Order_Region            nvarchar(100)    
     ,Order_State             nvarchar(100)    
     ,Order_Status            nvarchar(50)     
@@ -50,7 +51,7 @@ CREATE TABLE staging.DataCo_Raw (
     ,Product_Description     nvarchar(4000)   
     ,Product_Image           nvarchar(4000)   
     ,Product_Name            nvarchar(255)    
-    ,Product_Price           decimal(38,10)    
+    ,Product_Price           decimal(18, 4)    
     ,Product_Status          nvarchar(50)     
     ,Shipping_Date           datetime           -- shipping date (DateOrders)
     ,Shipping_Mode           nvarchar(50)     NULL
